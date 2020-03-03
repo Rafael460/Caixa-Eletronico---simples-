dcead7cc548d123b83163eba8bb612261f1313fd
Rafael460 <60758319+Rafael460@users.noreply.github.com>
2020-03-03 09:57:14 -0300

    Formatação de Código
    Formatei o código.

--- Conta.java
+++ Conta.java
@@ -3,100 +3,97 @@ package eletronicocaixa;
 import java.util.Scanner;
 
 public class Conta {
-	
-	private String nome;
-	private  int  conta,saques;
-	private double saldo;
-	
-	Scanner entrada = new Scanner( System.in);
-	
-public Conta(String nome, int conta,  double saldo_inicial) {
-	this.nome= nome;
-	this.conta= conta;
-	saldo=saldo_inicial;
-	saques=0;
-}
+
+ private String nome;
+ private int conta, saques;
+ private double saldo;
+
+ Scanner entrada = new Scanner(System.in);
+
+ public Conta(String nome, int conta, double saldo_inicial) {
+  this.nome = nome;
+  this.conta = conta;
+  saldo = saldo_inicial;
+  saques = 0;
+ }
 
  public void extrato() {
-	 System.out.println("\tEXTRATO");
-	 System.out.println("Nome:"+ this.nome);
-	 System.out.println("Número da conta:"+ this.conta);
-	 System.out.printf(" Saldo atual: %.2f\n",this.saldo);
-	 System.out.println("Saques feito hoje: " + this.saques + "\n");
+  System.out.println("\tEXTRATO");
+  System.out.println("Nome:" + this.nome);
+  System.out.println("Número da conta:" + this.conta);
+  System.out.printf(" Saldo atual: %.2f\n", this.saldo);
+  System.out.println("Saques feito hoje: " + this.saques + "\n");
  }
- 
- public void  sacar (double valor) {
-	 if(saldo >= valor) {
-		 saldo -= valor;
-		 saques++;
-	System.out.println("Sacado: " + valor);
-	System.out.println("Novo Saldo: " + saldo + "\n");
-	 } else {
-		 System.out.println("Saldo insuficiente, Faça um depósito \n");
-	 }
+
+ public void sacar(double valor) {
+  if (saldo >= valor) {
+   saldo -= valor;
+   saques++;
+   System.out.println("Sacado: " + valor);
+   System.out.println("Novo Saldo: " + saldo + "\n");
+  } else {
+   System.out.println("Saldo insuficiente, Faça um depósito \n");
+  }
  }
- public void  depositar (double valor)
- {
-	 saldo += valor;
-	 
-	 System.out.println("Depositado: "+valor);
-	 System.out.println("Novo Saldo: "+ saldo + "\n");
+ public void depositar(double valor) {
+  saldo += valor;
+
+  System.out.println("Depositado: " + valor);
+  System.out.println("Novo Saldo: " + saldo + "\n");
  }
- 
+
  public void iniciar() {
-	 int opcao;
-	 
-	 do {
-		 exibeMenu();
-		 opcao = entrada.nextInt();
-		 escolheOpcao(opcao);
-	 }while(opcao!=4);
+  int opcao;
+
+  do {
+   exibeMenu();
+   opcao = entrada.nextInt();
+   escolheOpcao(opcao);
+  } while (opcao != 4);
  }
- 
- 
- public void exibeMenu(){
-	 System.out.println("\t Escolha a opção desejada: ");
-	 System.out.println("1 - Consultar Extrato");
-	 System.out.println("2 - Sacar");
-	 System.out.println("3 - Depositar");
-	 System.out.println("4 - Sair\n");
-	 System.out.println("Opção");
-		 
+
+
+ public void exibeMenu() {
+  System.out.println("\t Escolha a opção desejada: ");
+  System.out.println("1 - Consultar Extrato");
+  System.out.println("2 - Sacar");
+  System.out.println("3 - Depositar");
+  System.out.println("4 - Sair\n");
+  System.out.println("Opção");
+
  }
- 
+
  public void escolheOpcao(int opcao) {
-	 double valor;
-	 
-	 switch(opcao) {
-	 case 1:
-		 extrato();
-		 	break;
-		 	
-	 case 2:
-		 	if(saques<3){
-		 		System.out.println(" Quanto deseja Sacar:");
-		 		valor =  entrada.nextDouble();
-		 		sacar(valor);
-		 	}else {
-		 		System.out.println("Límite de saque diário atingidos. \n");
-		 	}
-		 	break;
-		 	
-	 case 3:
-		 System.out.println("Quanto deseja depositar: ");
-		 valor = entrada.nextDouble();
-		 depositar(valor);
-		 break;
-		 
-	 case 4:
-		 System.out.println("Sistema encerrado.");
-		 break;
-		 
-	 default:
-		 System.out.println(" Opção Inválida");
-		 	}
-		 		
-		 	}
+  double valor;
+
+  switch (opcao) {
+   case 1:
+    extrato();
+    break;
+
+   case 2:
+    if (saques < 3) {
+     System.out.println(" Quanto deseja Sacar:");
+     valor = entrada.nextDouble();
+     sacar(valor);
+    } else {
+     System.out.println("Límite de saque diário atingidos. \n");
+    }
+    break;
+
+   case 3:
+    System.out.println("Quanto deseja depositar: ");
+    valor = entrada.nextDouble();
+    depositar(valor);
+    break;
+
+   case 4:
+    System.out.println("Sistema encerrado.");
+    break;
+
+   default:
+    System.out.println(" Opção Inválida");
+  }
+
+ }
 }
- 
- 
